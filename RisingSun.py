cd4ccf3609bdb8f7206163eb3b24ed4aacf53945
YOUNG-SHINNICK JHAMNI M <jyou1904@education.mpls.k12.mn.us>
2017-10-27 10:05:23 -0500

    trying to unbreak stuff

--- RisingSun.py
+++ RisingSun.py
@@ -4,3 +4,10 @@
 #RisingSun.py
 #Interactive Animation w/rising Sun
 ###
+import Tkinter as tk #names TKinter to be used as tk
+root = tk.Tk() #Creates root window
+canvas = tk.Canvas(root, width=300, height=300, borderwidth=0, highlightthickness=0, bg="white")
+canvas.grid() #Fills background white and creates a larger Canvas
+#format (x0, y0, x1, y1), all are poits for the circle's box
+canvas.create_oval(0,300, 0, 300, width=2, fill='red')
+root.mainloop()# Event loop
